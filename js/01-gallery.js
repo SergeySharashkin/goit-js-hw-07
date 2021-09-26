44c7b63dcf7524b0f087dd90556bda2bc6982010
SergeySharashkin <S_sharashkin@mail.ru>
2021-09-26 23:53:28 +0300

    ыы

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+galleryEl.addEventListener("click", onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  const isBigImg = evt.target.classList.contains("gallery__image");
+  if (!isBigImg) {
+    return;
+  }
+  console.log(evt.target.dataset.source);
+}
+// event.preventDefault()
+// document.querySelector('button.image').onclick = () => {
+
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`).show()
+
+// }
+/* <button class="image">img</button> */
